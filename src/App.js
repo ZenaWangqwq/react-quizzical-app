8ab204b68ee1aab3ce153e131c746e367af019e8
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-17 17:09:26 +0100

    add intro-page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function App() {
+    return (
+        <main>
+            <div className="start-screen">
+                <h1 className="app-name">Quizzical</h1>
+                <p className="description">Start the quiz to test your random knowledge for fun!</p>
+                <button className="start-button">Start quiz</button>
+            </div>
+        </main>
+    )
+}
